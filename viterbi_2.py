c904986e93a9ffbe417667835549e1a184e2f01c
Mohamed Amn <mamn2@illinois.edu>
2021-04-01 20:22:22 -0500

    viterbi 2

--- viterbi_2.py
+++ viterbi_2.py
@@ -11,9 +11,179 @@
 # Modified Spring 2021 by Kiran Ramnath (kiranr2@illinois.edu)
 
 """
-Part 3: Here you should improve viterbi to use better laplace smoothing for unseen words
-This should do better than baseline and your first implementation of viterbi, especially on unseen words
+Part 2: This is the simplest version of viterbi that doesn't do anything special for unseen words
+but it should do better than the baseline at words with multiple tags (because now you're using context
+to predict the tag).
 """
+
+import math
+import numpy as np 
+
+
+def counts(train):
+
+    uniqueTags = set()
+    uniqueWords = set()
+    transitionCounts = {}
+    transitionCountTotals = {}
+    emissionCounts = {}
+    hapaxWords = set()
+    hapaxTags = {}    
+
+    for sentence in train:
+        
+        prevTag = 'START'
+        for curWord, curTag in sentence:
+
+            if curWord in hapaxWords:
+                hapaxWords.remove(curWord)
+                if hapaxTags[curTag] > 0:
+                    hapaxTags[curTag] -= 1
+            else:
+                hapaxWords.add(curWord)
+                if curTag not in hapaxTags:
+                    hapaxTags[curTag] = 0
+                hapaxTags[curTag] += 1
+
+            uniqueTags.add(curTag)
+            uniqueWords.add(curWord)
+    
+            if curTag not in emissionCounts:
+                emissionCounts[curTag] = {}
+            if curWord in emissionCounts[curTag]:
+                emissionCounts[curTag][curWord] += 1
+            else:
+                emissionCounts[curTag][curWord] = 1
+
+            if prevTag not in transitionCounts:
+                transitionCounts[prevTag] = {}
+                transitionCountTotals[prevTag] = 0
+            if curTag in transitionCounts[prevTag]:
+                transitionCounts[prevTag][curTag] += 1
+            else:
+                transitionCounts[prevTag][curTag] = 1
+            transitionCountTotals[prevTag] += 1
+
+            prevTag = curTag
+
+    return uniqueTags, uniqueWords, transitionCounts, transitionCountTotals, emissionCounts, hapaxTags, len(hapaxWords)
+
+def calcTransProbs(transitionCounts, transitionCountTotals, uniqueTags, smoothingParam):
+
+    transitionProbs = {}
+    for prev in uniqueTags:
+        if prev not in transitionProbs:
+            transitionProbs[prev] = {}
+        for cur in uniqueTags:
+
+            if prev == 'END':
+                continue
+            if cur in transitionCounts[prev]:
+                transitionProbs[prev][cur] = math.log( (transitionCounts[prev][cur] + smoothingParam) / (transitionCountTotals[prev] + ( len(uniqueTags) + 1 ) * smoothingParam))
+            else:
+                transitionProbs[prev][cur] = math.log(smoothingParam / ( sum(transitionCountTotals.values()) + ( len(uniqueTags) + 1 ) * smoothingParam))
+
+    transitionProbs['UNSEEN'] = math.log(smoothingParam / (sum(transitionCountTotals.values()) + ( len(uniqueTags) + 1 ) * smoothingParam))
+
+    return transitionProbs
+    
+def calcEmissionProbs(emissionCounts, uniqueTags, uniqueWords, hapaxTags, numHapaxWords, smoothingParam):
+
+
+    hapaxProbs = {}
+
+    for tag in uniqueTags:
+        if tag in hapaxTags:
+            hapaxProbs[tag] = (hapaxTags[tag] + 5) / (numHapaxWords + len(uniqueTags) * smoothingParam)
+        else:
+            hapaxProbs[tag] = 5 / (numHapaxWords + len(uniqueTags) * smoothingParam)
+   
+
+    emissionProbs = {}
+    for word in uniqueWords:
+        for tag in uniqueTags:
+
+            if tag not in emissionProbs:
+                emissionProbs[tag] = {}
+            
+            if word in emissionCounts[tag]:
+                emissionProbs[tag][word] = math.log( (emissionCounts[tag][word] + smoothingParam) / ( sum(emissionCounts[tag].values()) + ( len(uniqueWords) + 1 ) * smoothingParam))
+            else:
+                emissionProbs[tag][word] = math.log( (smoothingParam) / ( sum(emissionCounts[tag].values()) + ( len(uniqueWords) + 1 ) * smoothingParam))
+
+    for tag in uniqueTags:
+        emissionProbs[tag]['UNSEEN'] = math.log( (hapaxProbs[tag] * smoothingParam) / ( sum(emissionCounts[tag].values()) + ( len(uniqueWords) + 1 ) * smoothingParam))
+
+    return emissionProbs
+
+def posTagging(sentence, tags, transitionProbs, emissionProbs):
+
+    viterbi = np.zeros((len(sentence), len(tags)))
+    backpointer = np.zeros((len(sentence), len(tags)))
+
+    initialProbs = transitionProbs['START']
+    for i in range(len(tags)):
+
+        tag = tags[i]
+        initialProbsTag = None
+        emissionsProbsTag = None
+
+        if tag in initialProbs:
+            initialProbsTag = initialProbs[tag]
+        else:
+            initialProbsTag = initialProbs['UNSEEN']
+
+        if tag in emissionProbs and sentence[0] in emissionProbs[tag]:
+            emissionsProbsTag = emissionProbs[tag][sentence[0]]
+        else:
+            emissionsProbsTag = emissionProbs[tag]['UNSEEN']
+
+        viterbi[0][i] = initialProbsTag + emissionsProbsTag
+
+    for i in range(1, len(sentence)):
+        word = sentence[i]
+        for j in range(len(tags)):
+
+            maxVal = None
+            argMax = None
+
+            emissionsProbability = None
+
+            if tags[j] in emissionProbs and word in emissionProbs[tags[j]]:
+                emissionsProbability = emissionProbs[tags[j]][word]
+            else:
+                emissionsProbability = emissionProbs[tags[j]]['UNSEEN']
+                
+            for k in range(len(tags)):
+
+                transitionProbability = None
+
+                if tags[k] in transitionProbs and tags[j] in transitionProbs[tags[k]]:
+                    transitionProbability = transitionProbs[tags[k]][tags[j]]
+                else:
+                    transitionProbability = transitionProbs['UNSEEN']
+
+                if maxVal is None or (emissionsProbability + transitionProbability + viterbi[i - 1][k]) > maxVal:
+                    maxVal = emissionsProbability + transitionProbability + viterbi[i - 1][k]
+                    argMax = k
+
+            viterbi[i][j] = maxVal
+            backpointer[i][j] = argMax
+
+    toReturn = []
+    lastWord = len(sentence) - 1
+    curMaxVal = None
+    curArgMax = None
+    for i in range(len(tags)):
+        if curMaxVal == None or viterbi[lastWord][i] > curMaxVal:
+            curMaxVal = viterbi[lastWord][i]
+            curArgMax = i
+    for i in range(lastWord, -1, -1):
+        toReturn = [(sentence[i], tags[curArgMax])] + toReturn
+        curArgMax = int(backpointer[i][curArgMax])
+
+    return toReturn
+
 
 def viterbi_2(train, test):
     '''
@@ -22,4 +192,10 @@ def viterbi_2(train, test):
     output: list of sentences with tags on the words
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    return []
+
+    smoothingParam = 0.0001
+    uniqueTags, uniqueWords, transitionCounts, transitionCountTotals, emissionCounts, hapaxTags, numHapaxWords = counts(train)
+    transitionProbs = calcTransProbs(transitionCounts, transitionCountTotals, uniqueTags, smoothingParam)
+    emissionProbs = calcEmissionProbs(emissionCounts, uniqueTags, uniqueWords, hapaxTags, numHapaxWords, smoothingParam)
+
+    return [posTagging(sentence, list(uniqueTags), transitionProbs, emissionProbs) for sentence in test]
